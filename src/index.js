e13deb8009eba8ef9ff73cd31ceb45b429f25bc6
Rojan <rojan108p@gmail.com>
2022-11-01 17:43:14 +1100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// ES6
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(Stuff(), document.getElementById("root"));
+
+function Stuff() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>This is a Paragrah</p>
+    </div>
+  );
+}
