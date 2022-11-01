d988b20e5e3000e4be8e3defe8f00b5d23868950
Rojan <rojan108p@gmail.com>
2022-11-01 22:19:50 +1100

    JS Expressions in JSX Practice

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const fName = "Rojan";
+//const lName = "LastName";
+const thisYear = new Date().getFullYear();
+
 ReactDOM.render(Stuff(), document.getElementById("root"));
 
 function Stuff() {
   return (
+    //JS Expressions in JSX Practice
     <div>
-      <h1>Hello World</h1>
-      <p>This is a Paragrah</p>
+      <h1 className="redHeading">Created by {fName}</h1>
+      <p>Copyright © {thisYear}</p>
     </div>
   );
 }
